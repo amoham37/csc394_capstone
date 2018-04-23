587523e467eaedd5558ae019b2a112bce82733c2
Justin McCormick <justinmccormick221@yahoo.com>
2018-04-23 16:35:06 -0500

    Justin - DB Setup Completed

---
+++ graduation_paths/graduation_paths/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DataBaseConfig(AppConfig):
+    name = 'graduation_paths'
--- graduation_paths/graduation_paths/settings.py
+++ graduation_paths/graduation_paths/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graduation_paths.apps.DataBaseConfig',
 ]
 
 MIDDLEWARE = [
